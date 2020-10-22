fcdf9f941cac7cb9a0922d98c0e84e6077a6f913
Gim, subin <vk0218@naver.com>
2020-10-22 14:25:42 +0900

    config파일 삭제

---
+++ DB/config_example.js
@@ -0,0 +1,13 @@
+module.exports = (
+	function() {
+	  return {
+	    host: "",
+	    user: "",
+	    password: "",
+			database: "",
+			waitForConnections: true,
+			connectionLimit: 30,
+			multipleStatements: true
+	  }
+  }
+)();
